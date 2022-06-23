8a0b3ae31989514c94a06c920b00241aedb17384
Rishi-Dave <rishipdave@gmail.com>
2022-06-23 11:14:34 -0700

    heroku stuff

--- ewaste/ewaste/settings.py
+++ ewaste/ewaste/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_1-a719=x!n-pxq5qv-wzo*+0r*njrb$z_o%olsaxu0$vc=ikr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 #Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'ewasteapp.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- ewaste/ewaste/urls.py
+++ ewaste/ewaste/urls.py
@@ -19,6 +19,9 @@ from ewasteapp import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('', views.home_page, name = 'home'),
     path('pickup/', views.pickup, name = 'pickup'),
@@ -32,4 +35,7 @@ urlpatterns = [
     path('driverview/', views.driverview, name = 'driverview'),
     path('delete/', views.delete, name = 'delete'),
     path('delivered/<obj_id>', views.delivered, name = 'delivered'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
