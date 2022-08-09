d509928b3738f150b939ca5273da81e4fd56f0d1
Rishi-Dave <rishipdave@gmail.com>
2022-08-09 15:17:15 -0700

    Update settings.py

--- ewaste/ewaste/settings.py
+++ ewaste/ewaste/settings.py
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'ewaste.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,8 +91,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
